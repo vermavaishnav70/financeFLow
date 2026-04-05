47f278b82cee9e85eb3a2b164dc651c2c6d2a96c
Vaishnav Verma <vermavaishnav70@gmail.com>
2026-04-05 14:58:48 +0530

    chore: update swagger server configuration to use relative path

--- backend/src/config/swagger.js
+++ backend/src/config/swagger.js
@@ -13,8 +13,8 @@ const options = {
     },
     servers: [
       {
-        url: 'http://localhost:3000',
-        description: 'Development server',
+        url: '/',
+        description: 'Current server',
       },
     ],
     components: {
